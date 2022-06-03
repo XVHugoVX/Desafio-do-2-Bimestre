8abc055e521f953fd48fe945fcd213b11a46f730
Hugo Chagas <102295946+XVHugoVX@users.noreply.github.com>
2022-06-03 20:13:23 -0300

    Add files via upload
    Desafio do 2º Bimestre de 2022 [Tentativa]
    Créditos do Canal: https://www.youtube.com/c/ThiCodes
    Que ajudou no estudo no código.

---
+++ Login/js/login.js
@@ -0,0 +1,81 @@
+let btn = document.querySelector('.fa-eye')
+
+btn.addEventListener('click', ()=>{
+  let inputSenha = document.querySelector('#senha')
+  
+  if(inputSenha.getAttribute('type') == 'password'){
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+function logar(){
+  let usuario = document.querySelector('#usuario')
+  let userLabel = document.querySelector('#userLabel')
+  
+  let senha = document.querySelector('#senha')
+  let senhaLabel = document.querySelector('#senhaLabel')
+  
+  let msgError = document.querySelector('#msgError')
+  let accounts = []
+  
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
+  
+  accounts = JSON.parse(localStorage.getItem('accounts'))
+  
+  accounts.forEach((item) => {
+    if(usuario.value == item.userCad && senha.value == item.senhaCad){
+       
+      userValid = {
+         nome: item.nomeCad,
+         user: item.userCad,
+         senha: item.senhaCad
+       }
+      
+    }
+  })
+   
+  if(usuario.value == userValid.user && senha.value == userValid.senha){
+    window.location.href = '../Program/Lista_de_Produtos.html'
+    
+    let mathRandom = Math.random().toString(16).substr(2)
+    let token = mathRandom + mathRandom
+    
+    localStorage.setItem('token', token)
+    localStorage.setItem('LoggedAccounts', JSON.stringify(userValid))
+  } else {
+    userLabel.setAttribute('style', 'color: red')
+    usuario.setAttribute('style', 'border-color: red')
+    senhaLabel.setAttribute('style', 'color: red')
+    senha.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    usuario.focus()
+  }
+  
+}
+
+let useraccount = Number(sessionStorage.getItem('logged'));
+
+const session = localStorage.getItem("session");
+
+LoggedOrNo ();
+
+function LoggedOrNo (){
+  if(session) {
+    sessionStorage.setItem("logged", session);
+    useraccount = session;
+  }
+
+  if(useraccount) {
+    window.location.href = "../Login/login.html"
+    return
+  }
+}
+
+console.log(useraccount);
